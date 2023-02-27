68d168cedfc9dddcd57132dee2dd15b8431924e7
Mica Beneke <14852919+bemica@users.noreply.github.com>
2023-02-27 10:49:21 +0100

    Update script to delete username instead

--- replace-codeowner.sh
+++ replace-codeowner.sh
@@ -2,36 +2,39 @@
 
 pushd "${1}" || exit
 
-# Go back to default branch and be up to date, switch to new branch
+# Go back to default branch and be up to date
 git stash
 DEFAULT_BRANCH=$(git remote show origin | awk '/HEAD branch/ {print $NF}')
 git switch "$DEFAULT_BRANCH"
 git pull
 git reset --hard "origin/$DEFAULT_BRANCH"
-git switch -c mica-has-a-new-name
 
 PR_NEEDED=false
 
-# Replace my name in CODEOWNERS files
+# Delete my name in CODEOWNERS files
 if test -f "CODEOWNERS"; then
-  cat CODEOWNERS | sed -e "s/bemica //g" > /tmp/CODEOWNERS
+  cat CODEOWNERS | sed -e "s/@bemica //g" | sed -e "s/ @bemica//g" > /tmp/CODEOWNERS
   cp /tmp/CODEOWNERS CODEOWNERS
   rm /tmp/CODEOWNERS
   if git status | grep -q "Changes not staged for commit"; then
+      git switch -c offboard-mica
       git add CODEOWNERS
       PR_NEEDED=true
     fi
 fi
 
 if test -f ".github/CODEOWNERS"; then
-  cat .github/CODEOWNERS | sed -e "s/bemica //g" > /tmp/CODEOWNERS
+  cat .github/CODEOWNERS | sed -e "s/@bemica //g" | sed -e "s/ @bemica//g" > /tmp/CODEOWNERS
   cp /tmp/CODEOWNERS .github/CODEOWNERS
   rm /tmp/CODEOWNERS
   if git status | grep -q "Changes not staged for commit"; then
+    git switch -c offboard-mica
     git add .github/CODEOWNERS
     PR_NEEDED=true
   fi
 fi
+
+#echo $PR_NEEDED
 
 # Commit and open PR
 if $PR_NEEDED; then
@@ -39,9 +42,9 @@ if $PR_NEEDED; then
   git push --set-upstream origin "offboard-mica"
   gh pr create --title "Offboard @bemica" --body "Remove Mica from CODEOWNERS" \
     --base "$DEFAULT_BRANCH" --head "offboard-mica"
+  git switch "$DEFAULT_BRANCH"
+  git branch -D "offboard-mica"
 fi
 
-git switch "$DEFAULT_BRANCH"
-git branch -D "mica-has-a-new-name"
 
 popd || exit
